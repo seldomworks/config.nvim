a818495ed022b0c44f56c6c37c2d9943893298b7
Anish Muttreja <amuttrej@qti.qualcomm.com>
2023-02-10 16:39:27 -0500

    font size

--- init.lua
+++ init.lua
@@ -48,8 +48,8 @@ require('nvim-treesitter.configs').setup {
 }
 
 require('orgmode').setup({
-  org_agenda_files = {'~/OneDrive - Qualcomm/org/*', '~/my-orgs/**/*'},
-  org_default_notes_file = '~/OneDrive - Qualcomm/org/refile.org',
+  org_agenda_files = {'~/org/*', '~/my-orgs/**/*'},
+  org_default_notes_file = '~/org/refile.org',
 })
 -- let s:fontsize = 14
 -- function! AdjustFontSize(amount)
